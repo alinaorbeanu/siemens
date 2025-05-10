9c670132d41bb94a72c4daf1c6667eef8f147fb0
alinaorbeanu <92021541+alinaorbeanu@users.noreply.github.com>
2025-05-10 14:50:11 +0300

    + added bean over item repository

--- SiemensJava2025/src/main/java/com/siemens/internship/repository/ItemRepository.java
+++ SiemensJava2025/src/main/java/com/siemens/internship/repository/ItemRepository.java
@@ -1,11 +1,14 @@
 package com.siemens.internship.repository;
 
 import com.siemens.internship.model.Item;
+import jakarta.transaction.Transactional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
+@Transactional
 public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("SELECT id FROM Item")
     List<Long> findAllIds();
